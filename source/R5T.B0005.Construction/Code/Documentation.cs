431b22cf33de38e405db6f290809ce1dbc47db47
davidcoats <david@getsafetycone.com>
2022-04-17 15:37:54 -0700

    Initial commit of source-stub.

---
+++ source/R5T.B0005.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0005.Construction
+{
+    /// <summary>
+    /// Construction project for Example and related extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0005
+{
+    /// <summary>
+    /// Example and related extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
